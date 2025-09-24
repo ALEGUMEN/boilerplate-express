119fce8bff673be93a785befa2a11f15b4897ac3
Alejandra Guardado <aleguamen_@hotmail.com>
2025-09-23 23:29:25 -0600

    json response

--- myApp.js
+++ myApp.js
@@ -1,12 +1,19 @@
-// Importa Express
 const express = require('express');
 const path = require('path');
 const app = express();
 
+// Middleware para servir archivos estáticos en /public
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// Ruta raíz: enviar index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// Nueva ruta API: responder con JSON en /json
+app.get('/json', (req, res) => {
+  res.json({ message: "Hello json" });
+});
+
+// Exportar la app
 module.exports = app;
