ff8628e0b4d6fc1aff59a0edcc87e9a03a33346e
Alejandra Guardado <aleguamen_@hotmail.com>
2025-09-23 22:05:06 -0600

    Fix Render deployment

--- myApp.js
+++ myApp.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const app = express();
 
-// Ruta raíz
 app.get('/', (req, res) => {
   res.send('Hello Express');
 });
 
-// Servir archivos estáticos (opcional si tienes /public)
-// app.use(express.static('public'));
-
-// Puerto dinámico para Render o local
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
 
 
 
