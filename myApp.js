0b474807fadf0fa201876ce8faeaec2518cc926e
Alejandra Guardado <aleguamen_@hotmail.com>
2025-09-24 11:19:14 -0600

    middleware

--- myApp.js
+++ myApp.js
@@ -1,10 +1,23 @@
-require('dotenv').config(); // <-- carga las variables de entorno
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
 
+// Middleware de logging
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
+// Middleware para archivos estáticos
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+// Ruta principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Ruta /json
 app.get('/json', (req, res) => {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === 'uppercase') {
@@ -13,10 +26,5 @@ app.get('/json', (req, res) => {
   res.json({ message });
 });
 
-//const PORT = 3000;
-//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-// Exportar la app
 module.exports = app;
 
