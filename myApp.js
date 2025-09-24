33bb6d5d33e76486c4b85bbfde75c102071cf534
Alejandra Guardado <aleguamen_@hotmail.com>
2025-09-24 10:18:21 -0600

    cuarto intento

--- myApp.js
+++ myApp.js
@@ -3,25 +3,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Middleware para servir archivos estáticos
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-// Ruta raíz: enviar index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Ruta API: responder con JSON en /json
 app.get('/json', (req, res) => {
   let message = "Hello json";
-
-  // Revisar variable de entorno
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
-
-  res.json({ message: message });
+  res.json({ message });
 });
+
 
 // Exportar la app
 module.exports = app;
