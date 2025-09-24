a917aecc92429f9926a5ce7e87d8b1b69b35a755
Alejandra Guardado <aleguamen_@hotmail.com>
2025-09-23 23:21:12 -0600

    add express.static

--- myApp.js
+++ myApp.js
@@ -1,13 +1,12 @@
 // Importa Express
 const express = require('express');
+const path = require('path');
 const app = express();
-const path = require('path'); // Para construir rutas seguras
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-
-
 
 module.exports = app;
